5affb796c1ac737e6da59ccb3c12a731e71e212b
okekedev <okekedev@outlook.com>
2025-11-03 13:10:52 -0600

    Initial Commit

---
+++ ChannelLink/ChannelLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChannelLinkApp.swift
+//  ChannelLink
+//
+//  Created by Christian  on 11/3/25.
+//
+
+import SwiftUI
+
+@main
+struct ChannelLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChannelLink/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChannelLink
+//
+//  Created by Christian  on 11/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
